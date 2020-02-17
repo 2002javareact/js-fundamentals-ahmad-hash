c5a2702522189f10b2de3926b5a23901ffa7ee97
ahmad-hash <aa223189077@gmail.com>
2020-02-17 16:44:27 -0500

    04

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,23 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+    
+if (sumNum<= 0 ||sumNum<= 1 ){
+return (1)
 }
+
+     for (let y = sumNum - 1; y >= 2; y--){
+         
+       sumNum *= y;
+      
+        
+              }
+       return sumNum;       
+     }
+    
+  console.log (factorial(5))
+
+
+/*Farctorial of x number is multiplied by itself and all the counting decrementing down till 0.
+ Moreover addition of every single pair which multiplied to each other.
+so x * x-i, i = 1, j = 2 x--; i++; j >= 0, i <= x*/
