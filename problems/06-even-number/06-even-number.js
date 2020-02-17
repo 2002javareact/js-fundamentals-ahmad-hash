91c89a934a6e69cef9fb631a4cbb61889a12c91b
ahmad-hash <aa223189077@gmail.com>
2020-02-17 13:49:30 -0500

    06

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,23 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    let i = 0
+for (; i <= someNum; i+=2 ) {
+
+ 
+ 
 }
+
+
+if (i===someNum) {
+    console.log(`The ${someNum} is even`);
+    
+    
+} else {
+    console.log(`The ${someNum} is ODD`);
+    
+    
+}
+return ''
+}
+console.log(isEven(9));
